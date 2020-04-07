e0c0a23364ec5fb0557a3f69f617c0ca0ff25e5a
James Gullickson <jgullick@umn.edu>
2020-04-06 20:21:40 -0500

    name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
       <div id='banner' className='px-5'>
-        <h1>Brew Locator</h1>
+        <h1>GeoHop</h1>
         <i></i>
       </div>
     )
